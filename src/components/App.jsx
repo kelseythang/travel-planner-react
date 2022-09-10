02aa28f29da8ab00b3db3bd16b6fe5c0eb78f4a1
Kelsey Thang <thang.kelsey@gmail.com>
2022-09-10 01:04:16 -0400

    creates LocationList component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/locations')
+      .then(res => res.json())
+      .then(data => setLocations(data));
+  }, [])
+
+  const test = locations.map(location => {
+    return (
+      <img key={location.name} src={location.image} />
+    )
+  })
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Test</h1>
+      {test}
+    </div>
   )
 }
 
