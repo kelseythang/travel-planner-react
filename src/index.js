718e34da64f70749b2c325cf1aff7a817cfebb2f
Kelsey Thang <thang.kelsey@gmail.com>
2022-09-20 16:23:40 -0400

    Updates index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-//import {StrictMode} from 'react';
+import {StrictMode} from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
- // <StrictMode>
+  <StrictMode>
     <App />
- // </StrictMode>
+  </StrictMode>
 );
